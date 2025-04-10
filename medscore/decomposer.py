332e291e8085f11dd1450ed4a7767424a5c46340
Alexandra DeLucia <aadelucia@gmail.com>
2025-04-10 18:16:01 -0400

    decomposer draft

---
+++ medscore/decomposer.py
@@ -0,0 +1,94 @@
+"""
+Decomposer
+"""
+
+import os
+from functools import partial
+import asyncio
+
+import jsonlines
+from tqdm import tqdm
+import backoff
+import requests
+from openai import AsyncOpenAI
+from openai.types.chat.chat_completion import ChatCompletion
+
+from utils import process_claim, parse_sentences, chunker
+from prompts import MEDSCORE_PROMPT
+
+
+class Decomposer(object):
+    def __init__(
+            self,
+            server_path: str,
+            model_name: str,
+            system_prompt: str = MEDSCORE_PROMPT,
+            random_state: int = 42,
+            batch_size: int = 32
+    ):
+        self.client = AsyncOpenAI(
+            server_path
+        )
+        self.model_name = model_name
+        self.system_prompt = system_prompt
+        self.random_state = random_state
+        self.batch_size = batch_size
+
+        self.agent = partial(
+            self.client.chat.completions.create,
+            model=self.model_name,
+            seed=self.random_state,
+            temperature=0.0,
+            top_p=1.0,
+            max_tokens=256
+        )
+
+    def __call__(self, decomp_input: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        # Prepare prompt and user input
+        messages = []
+        for d in decomp_input:
+            formatted_input = self.format_input(d['context'], d['sentence'])
+            messages.append([
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": formatted_input}
+            ])
+
+        # Async calls for batch_size items
+        all_completions = []
+        n_iter = len(messages) // self.batch_size
+        for batch in tqdm(chunker(messages, self.batch_size), desc="Decompose", total=n_iter):
+            completions = asyncio.run(self.batch_response(batch))
+            all_completions.extend(completions)
+
+        # Format claims
+        decompositions = []
+        for d_input, completion in zip(decomp_input, all_completions):
+            claim_list = completion.choices[0].message.content.split("\n")
+            claim_list = process_claim(claim_list)
+            for claim in claim_list:
+                decomp = {k:v for k,v in d_input.items()}
+                decomp["claim"] = claim
+                decompositions.append(decomp)
+            if not claim_list:
+                decomp = {k:v for k,v in d_input.items()}
+                decomp["claim"] = None
+                decompositions.append(decomp)
+        return decompositions
+
+    @backoff.on_exception(
+        backoff.expo,
+        requests.exceptions.RequestException,
+        max_time=60
+    )
+    async def batch_response(self, batch: List[List[Dict[str, str]]]) -> List[ChatCompletion]:
+        async_responses = [
+            self.agent(
+                messages=x,
+            )
+            for x in batch
+        ]
+        return await asyncio.gather(*async_responses)
+
+    def format_input(self, context: str, sentence: str) -> str:
+        return f"Context: {context}\nPlease breakdown the following sentence into independent facts: {sentence}\nFacts:\n"
+
---
+++ medscore/medscore.py
@@ -0,0 +1,103 @@
+"""
+Extract and verify claims with MedScore
+"""
+
+import os
+import sys
+import logging
+from typing import List, Any, Optional, Dict
+from argparse import ArgumentParser
+
+import jsonlines
+from tqdm import tqdm
+
+from utils import parse_sentences
+from decomposer import Decomposer
+
+
+class MedScore(object):
+    def __init__(
+            self,
+            model_name_decomposition: str,
+            server_decomposition: str,
+            model_name_verification: str,
+            server_verification: str,
+            verification_mode: str
+    ):
+        self.decomposer = Decomposer(
+            model_name=model_name_decomposition,
+            server_path=server_decomposition
+        )
+
+    def decompose(
+        self,
+        dataset: List[Dict[str, Any]],
+    ) -> List[Dict[str, Any]]:
+        # Split each response
+        decomposer_input = []
+        for item in dataset:
+            sentences = parse_sentences(item["response"])
+            for idx, sentence in enumerate(sentences):
+                decomposer_input.append({
+                    "id": item["id"],
+                    "sentence_id": idx,
+                    "context": item["response"],
+                    "sentence": sentence["text"],
+                })
+
+        # Decompose
+        decompositions = self.decomposer(decomposer_input)
+
+        # Re-sort
+        return decompositions
+
+    def verify(self, claim):
+        return
+
+
+#############
+# Main
+#############
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--input_file", required=True, type=str)
+    parser.add_argument("--output_dir", default="", type=str)
+    parser.add_argument("--model_name_decomposition", type=str, default="gpt-4o-mini")
+    parser.add_argument("--server_decomposition", type=str, default="https://api.openai.com")
+    parser.add_argument("--model_name_verification", type=str, default="gpt-4o-mini")
+    parser.add_argument("--server_verification", type=str, default="https://api.openai.com")
+    parser.add_argument("--verification_mode", type=str, choices=["internal", "medrag"], default="internal")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # Initialize MedScore
+    scorer = MedScore(
+        model_name_decomposition=args.model_name_decomposition,
+        server_decomposition=args.server_decomposition,
+        model_name_verification=args.model_name_verification,
+        server_verification=args.server_verification,
+        verification_mode=args.verification_mode
+    )
+
+    # Load data
+    with jsonlines.open(args.input_file) as reader:
+        dataset = [item for item in reader.iter()]
+
+    # Batch decompose
+    # Save intermediate
+    decompositions = scorer.decompose(dataset)
+    decomp_output_file = os.path.join(args.output_dir, "decompositions.jsonl")
+    with jsonlines.open(decomp_output_file, 'w') as writer:
+        writer.write_all(decompositions)
+
+    exit(0)
+
+    # Batch verify
+    output = scorer.verify(decompositions)
+    output_file = os.path.join(args.output_dir, "medscore_output.jsonl")
+    with jsonlines.open(output_file, 'w') as writer:
+        writer.write_all(output)
+
