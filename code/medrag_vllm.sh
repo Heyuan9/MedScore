fcd53439d8f8187c411f8eabb0b79cba2e9a2b00
Heyuan9 <hhuan134@jh.edu>
2025-05-23 19:29:33 -0400

    Update medrag_vllm.sh

--- code/medrag_vllm.sh
+++ code/medrag_vllm.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=medscore-internal
-#SBATCH --time=15:00:00
-#SBATCH --nodes=1
-#SBATCH --mem=300GB
-#SBATCH --partition=bigmem
-#SBATCH -A mdredze1_bigmem
-#SBATCH --output=logs/%x.%j.log
+### Put your bash setting script here
 
 module load anaconda
-conda activate medic
+### Create MedScore environment
+conda activate medscore
 
-cd /home/hhuan134/scr4_mdredze1/hhuan134/MedScore
-### GPT-4o internal
-# python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/PUMA_gpt4o/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/PUMA_gpt4o" --verify_only --verification_mode "internal" --model_name_verification "gpt-4o"
-
-## BioLLM70B internal
-python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/AskDocs_MedScore_OpenBioLLM/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/AskDocs_MedScore_OpenBioLLM" --verify_only --verification_mode "internal" --model_name_verification "aaditya/Llama3-OpenBioLLM-70B" --server_verification "https://router.huggingface.co/nebius/v1"
-# python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/AskDocs_VeriScore_OpenBioLLM/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/AskDocs_VeriScore_OpenBioLLM" --verify_only --verification_mode "internal" --model_name_verification "aaditya/Llama3-OpenBioLLM-70B" --server_verification "http://icgpu02:22659/v1"
-
-
-### MedRAG
-# export MEDRAG_CORPUS="/home/hhuan134/scr4_mdredze1/medic_project/data/MedCorp"
-
-# python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_QA/PUMA_medrag/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_QA/PUMA_medrag" --verify_only --verification_mode "medrag" --model_name_verification "mistralai/Mistral-Small-24B-Instruct-2501" --server_verification "http://icgpu05:22659/v1"
-
-### MedAlpaca-7B
-# python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_QA/PUMA_medalpaca/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_QA/PUMA_medalpaca" --verify_only --verification_mode "internal" --model_name_verification "medalpaca/medalpaca-7b" --server_verification "http://icgpu02:22659/v1"
-# python -m medscore.medscore --input_file "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_nonQA/AskDocs_MedAlpaca/decompositions.jsonl" --output_dir "/home/hhuan134/scr4_mdredze1/hhuan134/MedScore/result/VeriScore_nonQA/AskDocs_MedAlpaca" --verify_only --verification_mode "internal" --model_name_verification "medalpaca/medalpaca-7b" --server_verification "http://icgpu02:22659/v1"
+cd ./MedScore
+### GPT-4o internal knowledge test
+python -m medscore.medscore --input_file "Your folder to /decompositions.jsonl" --output_dir "The output folder path" --verify_only --verification_mode "internal" --model_name_verification "gpt-4o"
